83901a2e61752d1db53255ab830b08116e5c7533
kawamura.hryk <kawamura.hryk@gmail.com>
2015-01-03 13:02:32 +0900

    refs #9

--- apps/settings/heroku.py
+++ apps/settings/heroku.py
@@ -4,7 +4,25 @@ heroku用設定ファイル
 """
 from apps.settings.base import *  # NOQA
 
-import dj_database_url
-DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
-
 INSTALLED_APPS += ('gunicorn', )
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+#DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+#import os
+#BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
